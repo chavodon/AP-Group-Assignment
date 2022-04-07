af56245d3422addfea98b92ea2002b85dad9ed6c
l3fl45h <48228330+l3fl45h@users.noreply.github.com>
2022-04-07 16:08:53 -0500

    Update EmployeePortal.java

--- src/gui/EmployeePortal.java
+++ src/gui/EmployeePortal.java
@@ -1,9 +1,8 @@
-package gui;
+package employee;
 
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -17,11 +16,10 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.KeyStroke;
-import javax.swing.event.InternalFrameEvent;
 
 import gui.CompleteSolution;
-import gui.ViewAllComplaint;
 import gui.ViewResolvedComplaint;
+import gui.ViewSpecificComplaint;
 
 public class EmployeePortal extends JFrame implements ActionListener {
 	
@@ -35,6 +33,10 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	private JButton viewBtn;
 	private JButton editBtn;
 	private JButton vrcBtn;
+	private JButton custBtn;
+	private JButton prodBtn;
+	private JButton billBtn;
+	private JButton genBtn;
 
 	private JFrame frame = new JFrame("CLEGS EMPLOYEE PORTAL");
 	private JLabel label;
@@ -50,130 +52,72 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	private JLabel label11;
 	private JLabel label12;
 	private JLabel label13;
-	private JLabel label14;
-	private JLabel label15;
-	private JLabel label16;
-	private JLabel label17;
 	
 	public EmployeePortal() {
-		label = new JLabel("CLEGS DASHBOARD");
-	    label.setBounds(500,25,200,20);
+		label = new JLabel("EMPLOYEE DASHBOARD");
+	    label.setBounds(500,25,250,20);
 	    label.setFont(new Font("Ariel", Font.BOLD, 18));
-	    label.setBackground(Color.black);
-	    label.setForeground(Color.white);
-	    label.setOpaque(true);
+	    label.setForeground(Color.black);
 	    
 	    label2 = new JLabel("Complaint Information");
 	    label2.setBounds(775,85,250,20);
 	    label2.setFont(new Font("Ariel", Font.BOLD, 18));
-	    label2.setBackground(Color.black);
-	    label2.setForeground(Color.white);
-	    label2.setOpaque(true);
+	    label2.setForeground(Color.black);
 	    
 	    label3 = new JLabel("Services");
 	    label3.setBounds(250,85,100,20);
 	    label3.setFont(new Font("Ariel", Font.BOLD, 18));
-	    label3.setBackground(Color.black);
-	    label3.setForeground(Color.white);
-	    label3.setOpaque(true);
+	    label3.setForeground(Color.black);
 	    
 	    label4 = new JLabel("Resolved Complaints");
 	    label4.setBounds(725,120,150,20);
 	    label4.setFont(new Font("Ariel", Font.BOLD, 12));
-	    label4.setBackground(Color.black);
-	    label4.setForeground(Color.white);
-	    label4.setOpaque(true);
+	    label4.setForeground(Color.black);
 	    
 	    label5 = new JLabel("Outstanding Complaints");
 	    label5.setBounds(920,120,150,20);
 	    label5.setFont(new Font("Ariel", Font.BOLD, 12));
-	    label5.setBackground(Color.black);
-	    label5.setForeground(Color.white);
-	    label5.setOpaque(true);
-	    
-	    label6 = new JLabel("Customer Service");
-	    label6.setBounds(525,155,150,20);
+	    label5.setForeground(Color.black);
+	    	    
+	    label6 = new JLabel("10");
+	    label6.setBounds(778,170,20,20);
 	    label6.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label6.setBackground(Color.black);
-	    label6.setForeground(Color.white);
-	    label6.setOpaque(true);
+	    label6.setForeground(Color.black);
 	    
-	    label7 = new JLabel("Product/Service");
-	    label7.setBounds(525,255,150,20);
+	    label7 = new JLabel("10");
+	    label7.setBounds(978,170,20,20);
 	    label7.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label7.setBackground(Color.black);
-	    label7.setForeground(Color.white);
-	    label7.setOpaque(true);
+	    label7.setForeground(Color.black);
 	    
-	    label8 = new JLabel("Bill/Payment");
-	    label8.setBounds(525,355,150,20);
+	    label8 = new JLabel("20");
+	    label8.setBounds(778,270,20,20);
 	    label8.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label8.setBackground(Color.black);
-	    label8.setForeground(Color.white);
-	    label8.setOpaque(true);
+	    label8.setForeground(Color.black);
 	    
-	    label9 = new JLabel("General");
-	    label9.setBounds(525,455,150,20);
+	    label9 = new JLabel("25");
+	    label9.setBounds(978,270,20,20);
 	    label9.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label9.setBackground(Color.black);
-	    label9.setForeground(Color.white);
-	    label9.setOpaque(true);
+	    label9.setForeground(Color.black);
 	    
-	    label10 = new JLabel("10");
-	    label10.setBounds(778,155,20,20);
+	    label10 = new JLabel("30");
+	    label10.setBounds(778,370,20,20);
 	    label10.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label10.setBackground(Color.black);
-	    label10.setForeground(Color.white);
-	    label10.setOpaque(true);
+	    label10.setForeground(Color.black);
 	    
-	    label11 = new JLabel("10");
-	    label11.setBounds(978,155,20,20);
+	    label11 = new JLabel("35");
+	    label11.setBounds(978,370,20,20);
 	    label11.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label11.setBackground(Color.black);
-	    label11.setForeground(Color.white);
-	    label11.setOpaque(true);
+	    label11.setForeground(Color.black);
 	    
-	    label12 = new JLabel("20");
-	    label12.setBounds(778,255,20,20);
+	    label12 = new JLabel("40");
+	    label12.setBounds(778,470,20,20);
 	    label12.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label12.setBackground(Color.black);
-	    label12.setForeground(Color.white);
-	    label12.setOpaque(true);
+	    label12.setForeground(Color.black);
 	    
-	    label13 = new JLabel("25");
-	    label13.setBounds(978,255,20,20);
+	    label13 = new JLabel("45");
+	    label13.setBounds(978,470,20,20);
 	    label13.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label13.setBackground(Color.black);
-	    label13.setForeground(Color.white);
-	    label13.setOpaque(true);
-	    
-	    label14 = new JLabel("30");
-	    label14.setBounds(778,355,20,20);
-	    label14.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label14.setBackground(Color.black);
-	    label14.setForeground(Color.white);
-	    label14.setOpaque(true);
-	    
-	    label15 = new JLabel("35");
-	    label15.setBounds(978,355,20,20);
-	    label15.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label15.setBackground(Color.black);
-	    label15.setForeground(Color.white);
-	    label15.setOpaque(true);
-	    
-	    label16 = new JLabel("40");
-	    label16.setBounds(778,455,20,20);
-	    label16.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label16.setBackground(Color.black);
-	    label16.setForeground(Color.white);
-	    label16.setOpaque(true);
-	    
-	    label17 = new JLabel("45");
-	    label17.setBounds(978,455,20,20);
-	    label17.setFont(new Font("Ariel", Font.BOLD, 15));
-	    label17.setBackground(Color.black);
-	    label17.setForeground(Color.white);
-	    label17.setOpaque(true);
+	    label13.setForeground(Color.black);
 	    
 	    frame.add(label);
 	    frame.add(label2);
@@ -188,24 +132,26 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	    frame.add(label11);
 	    frame.add(label12);
 	    frame.add(label13);
-	    frame.add(label14);
-	    frame.add(label15);
-	    frame.add(label16);
-	    frame.add(label17);
 	    
 		frame.setLayout(null);
+		frame.getContentPane().setBackground(new Color(160, 160, 160));
+		frame.setResizable(false);
 		frame.setSize(new Dimension(1200,600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		frame.getContentPane().setBackground(Color.BLACK);
+		//frame.getContentPane().setBackground(Color.BLACK);
 		
 		//--------------CREATE BUTTONS----------------------
 		
 		viewBtn = new JButton("VIEW ALL COMPLAINTS");
 		editBtn= new JButton("EDIT SOLUTION");
 		vrcBtn = new JButton("RESOLVED COMPLAINTS");
+		custBtn = new JButton("CUSTOMER SERVICE");
+		prodBtn = new JButton("PRODUCT/SERVICE");
+		billBtn = new JButton("BILL/PAYMENT");
+		genBtn = new JButton("GENERAL");
 		
-		
+		//---------------BUTTON DISPLAY----------------------
 		viewBtn.setBackground(Color.blue);
 		viewBtn.setForeground(Color.yellow);
 		viewBtn.setOpaque(true);
@@ -222,20 +168,51 @@ public class EmployeePortal extends JFrame implements ActionListener {
 		vrcBtn.setForeground(Color.yellow);
 		vrcBtn.setOpaque(true);
 		
-		viewBtn.setBounds(175,140,220,50);
-		editBtn.setBounds(175,240,220,50);
-		vrcBtn.setBounds(175,340,220,50);	
+		custBtn.setBackground(Color.blue);
+		custBtn.setForeground(Color.yellow);
+		custBtn.setOpaque(true);
+		
+		prodBtn.setBackground(Color.blue);
+		prodBtn.setForeground(Color.yellow);
+		prodBtn.setOpaque(true);
+		
+		billBtn.setBackground(Color.blue);
+		billBtn.setForeground(Color.yellow);
+		billBtn.setOpaque(true);
+		
+		genBtn.setBackground(Color.blue);
+		genBtn.setForeground(Color.yellow);
+		genBtn.setOpaque(true);
+		
+		//--------------BUTTON BOUNDS---------------------
+		
+		viewBtn.setBounds(175,155,220,50);
+		editBtn.setBounds(175,255,220,50);
+		vrcBtn.setBounds(175,355,220,50);
+		custBtn.setBounds(505,155,200,50);
+		prodBtn.setBounds(505,255,200,50);
+		billBtn.setBounds(505,355,200,50);
+		genBtn.setBounds(505,455,200,50);
 		
 		//-----------------ADD LISTENERS---------------------
 		viewBtn.addActionListener(this);
 		editBtn.addActionListener(this);
 		vrcBtn.addActionListener(this);
+		custBtn.addActionListener(this);
+		prodBtn.addActionListener(this);
+		billBtn.addActionListener(this);
+		genBtn.addActionListener(this);
 
 		//-----------------ADD / DISPLAY BUTTONS ON SCREEN-----------------------
 		frame.add(viewBtn);
 		frame.add(editBtn);
 		frame.add(vrcBtn);
-
+		frame.add(custBtn);
+		frame.add(prodBtn);
+		frame.add(billBtn);
+		frame.add(genBtn);
+		frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
 		navbar();
 	
 	}
@@ -300,7 +277,10 @@ public class EmployeePortal extends JFrame implements ActionListener {
 		} 	
 		else if (e.getSource() == vrcBtn) {
 			ViewResolvedComplaint vrc = new ViewResolvedComplaint();	
-		}		
+		}
+		else if (e.getSource() == custBtn) {
+			ViewSpecificComplaint vsc = new ViewSpecificComplaint();
+		}
 	}
 
 	public static void main(String[] args) {
