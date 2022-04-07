55dc62786b0f999bec217a305a78fa9345417ce0
l3fl45h <48228330+l3fl45h@users.noreply.github.com>
2022-04-06 21:42:51 -0500

    Update EmployeePortal.java

--- src/gui/EmployeePortal.java
+++ src/gui/EmployeePortal.java
@@ -3,6 +3,7 @@ package employee;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,12 +17,16 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
 import javax.swing.KeyStroke;
+import javax.swing.event.InternalFrameEvent;
+
+import gui.CompleteSolution;
+import gui.ViewAllComplaint;
+import gui.ViewResolvedComplaint;
 
 public class EmployeePortal extends JFrame implements ActionListener {
 	
 	//Declare global variables
 	private static final long serialVersionUID = 1L;
-	//JPanel jp = new JPanel();
 	public JMenuBar menuBar;
 	public JMenu menu, subMenu;
 	public JMenuItem menuItem;
@@ -43,6 +48,12 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	private JLabel label9;
 	private JLabel label10;
 	private JLabel label11;
+	private JLabel label12;
+	private JLabel label13;
+	private JLabel label14;
+	private JLabel label15;
+	private JLabel label16;
+	private JLabel label17;
 	
 	public EmployeePortal() {
 		label = new JLabel("CLEGS DASHBOARD");
@@ -122,6 +133,48 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	    label11.setForeground(Color.white);
 	    label11.setOpaque(true);
 	    
+	    label12 = new JLabel("20");
+	    label12.setBounds(778,255,20,20);
+	    label12.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label12.setBackground(Color.black);
+	    label12.setForeground(Color.white);
+	    label12.setOpaque(true);
+	    
+	    label13 = new JLabel("25");
+	    label13.setBounds(978,255,20,20);
+	    label13.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label13.setBackground(Color.black);
+	    label13.setForeground(Color.white);
+	    label13.setOpaque(true);
+	    
+	    label14 = new JLabel("30");
+	    label14.setBounds(778,355,20,20);
+	    label14.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label14.setBackground(Color.black);
+	    label14.setForeground(Color.white);
+	    label14.setOpaque(true);
+	    
+	    label15 = new JLabel("35");
+	    label15.setBounds(978,355,20,20);
+	    label15.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label15.setBackground(Color.black);
+	    label15.setForeground(Color.white);
+	    label15.setOpaque(true);
+	    
+	    label16 = new JLabel("40");
+	    label16.setBounds(778,455,20,20);
+	    label16.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label16.setBackground(Color.black);
+	    label16.setForeground(Color.white);
+	    label16.setOpaque(true);
+	    
+	    label17 = new JLabel("45");
+	    label17.setBounds(978,455,20,20);
+	    label17.setFont(new Font("Ariel", Font.BOLD, 15));
+	    label17.setBackground(Color.black);
+	    label17.setForeground(Color.white);
+	    label17.setOpaque(true);
+	    
 	    frame.add(label);
 	    frame.add(label2);
 	    frame.add(label3);
@@ -133,7 +186,13 @@ public class EmployeePortal extends JFrame implements ActionListener {
 	    frame.add(label9);
 	    frame.add(label10);
 	    frame.add(label11);
-		
+	    frame.add(label12);
+	    frame.add(label13);
+	    frame.add(label14);
+	    frame.add(label15);
+	    frame.add(label16);
+	    frame.add(label17);
+	    
 		frame.setLayout(null);
 		frame.setSize(new Dimension(1200,600));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
