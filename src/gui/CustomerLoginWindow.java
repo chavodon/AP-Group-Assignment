d26057c6b897af6093d8ea8e28028fc21e9a6389
l3fl45h <48228330+l3fl45h@users.noreply.github.com>
2022-04-08 05:51:05 -0500

    Update CustomerLoginWindow.java

--- src/gui/CustomerLoginWindow.java
+++ src/gui/CustomerLoginWindow.java
@@ -1,25 +1,19 @@
 package gui;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class CustomerLoginWindow extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-	private JFrame frame;
 	JLabel title;
 	private JLabel usernameLbl;
 	private JLabel passwordLbl;
@@ -27,12 +21,6 @@ public class CustomerLoginWindow extends JFrame {
 	private JPasswordField passwordTxt;
 	private JButton submitBtn;
 	private JButton bckBtn;
-	
-	private JLabel welcomeLabel;
-	
-	//Create an object of the GridBagConstraints/ Settings
-	
-	private GridBagConstraints gbc;
 	
 	public CustomerLoginWindow()
 	{
@@ -84,6 +72,7 @@ public class CustomerLoginWindow extends JFrame {
 			@Override
 			public void actionPerformed(ActionEvent e) 
 			{
+				dispose();
 				new CustomerDashboard();
 			}
 		});
@@ -91,140 +80,28 @@ public class CustomerLoginWindow extends JFrame {
 		add(submitBtn);
 		
 		bckBtn = new JButton("Back");
-		bckBtn.setForeground(Color.blue);
-		bckBtn.setBackground(Color.blue);
-		//gbc = new GridBagConstraints();
+		bckBtn.setBackground(new Color(96, 96, 96));
+		bckBtn.setFont(new Font ("Serif", Font.BOLD, 18));
+		bckBtn.setForeground(Color.black);
+		bckBtn.setBounds(192, 324, 180, 40);
+		bckBtn.addActionListener(new ActionListener() 
+		{
+			@Override
+			public void actionPerformed(ActionEvent e) 
+			{
+				dispose();
+				new WelcomeWindow();
+			}
+		});
 		
-		//welcomeLabel= new JLabel("WELCOME");
+		add(bckBtn);
 		setVisible(true);
-		
-//		bckBtn.addActionListener(this);
-//		button.addActionListener(this);	
-		//layoutComponents();
 	}
-	
-//	private void layoutComponents()
-//	{
-//		//Set the Layout Manager for the frame
-//		frame.setLayout(new GridBagLayout());
-//		
-//		welcomeLabel.setText("Customer Login");
-//		welcomeLabel.setFont(new Font("Serif", Font.BOLD, 20));
-//		welcomeLabel.setBackground(Color.BLACK);
-//		welcomeLabel.setOpaque(true);
-//		//Font f = welcomeLabel.getFont();
-//		//welcomeLabel.setFont(f.deriveFont(f.getStyle() | Font.BOLD));
-//		welcomeLabel.setForeground(Color.ORANGE);
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 0;
-//		gbc.gridy = 0;
-//		gbc.gridwidth = 0;
-//		gbc.anchor = GridBagConstraints.NORTH;
-//		gbc.insets = new Insets(-75,50,2,2);
-//		frame.add(welcomeLabel, gbc);
-//		
-//		usernameLbl.setSize(150,20);
-//		//Set size of the label for each components re=initalize the gridbag constraints
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 0;
-//		//Set which column on the X-Axis the first component should go
-//		gbc.gridy = 0;
-//		//Set which row on the y-axis the first component should go
-//		gbc.gridwidth = 1;
-//		//Set how many colums the component should span
-//		
-//		//Set where in the frame the component should be located
-//		gbc.anchor = GridBagConstraints.WEST;
-//		
-//		//The inset field specifies the extername padding of the component, the minimum
-//		// The amount of space between the component and the edges of its display area
-//		gbc.insets = new Insets(9,10,0,0);
-//		
-//		//Add the component to the frame with all its contraints
-//		frame.add(usernameLbl, gbc);
-//		
-//		gbc = new GridBagConstraints();
-//		//Re-Initialize the gridbag
-//		gbc.gridx = 3;
-//		gbc.gridy = 0;
-//		gbc.gridwidth = 3;
-//		gbc.gridheight = 2;
-//		//Set the internal padding for the components width
-//		gbc.ipadx = 0;
-//		gbc.anchor = GridBagConstraints.NORTHWEST;
-//		//The insets field specified the external padding of the component, the minimum
-//		//Amount of space between the component and the edges of its display area
-//		gbc.insets = new Insets(11,7,0,10);
-//		
-//		//Add the component to the frame with all its constraints
-//		frame.add(usernameTxt, gbc);
-//		
-//		passwordLbl.setText("Password:");
-//		passwordLbl.setForeground(Color.white);
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 0;
-//		gbc.gridy = 1;
-//		gbc.gridwidth = 1;
-//		gbc.anchor = GridBagConstraints.NORTHWEST;
-//		gbc.insets = new Insets(9,10,5,5);
-//		frame.add(passwordLbl, gbc);
-//		
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 3;
-//		gbc.gridy = 1;
-//		gbc.gridwidth = 3;
-//		gbc.gridheight = 2;
-//		gbc.ipadx = 0;
-//		gbc.anchor = GridBagConstraints.NORTHWEST;
-//		gbc.insets = new Insets(11,7,3,10);
-//		frame.add(passwordTxt, gbc);
-//		
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 4;
-//		gbc.gridy = 4;
-//		gbc.gridwidth = 6;
-//		gbc.ipadx = 40;
-//		gbc.ipady = 6;
-//		gbc.anchor = GridBagConstraints.CENTER;
-//		gbc.insets = new Insets(17,0,0,0);
-//		frame.add(submitBtn, gbc);
-//		
-//		gbc = new GridBagConstraints();
-//		gbc.gridx = 4;
-//		gbc.gridy = 4;
-//		gbc.gridwidth = 1;
-//		gbc.ipadx = 4;
-//		gbc.ipady = 4;
-//		gbc.anchor = GridBagConstraints.CENTER;
-//		gbc.insets = new Insets(17,-10,-3,-3);
-//		frame.add(bckBtn, gbc);
-//		
-//		frame.setSize(new Dimension(550,450));
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setVisible(true);
-//		frame.getContentPane().setBackground(Color.BLACK);
-//	}
-	
-	@SuppressWarnings("unused")
-	public void actionPerformed(ActionEvent e) {
-		//IF statement to select or determine what specific method to execute if the user clicks a particular button.
-
-				if (e.getSource() == submitBtn) {
-					EmployeePortal emp = new EmployeePortal();
-					//validate password
-				} 
-				
-			else if (e.getSource() == bckBtn) {
-					WelcomeWindow ww = new WelcomeWindow();	
-				} 
-
-				
-			}  
 
 	public static void main(String args[]){
-        
-		  new CustomerLoginWindow();
-	       
-	     }
+    
+	  new CustomerLoginWindow();
+       
+     }
 		
 }
